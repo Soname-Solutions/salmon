15764c4318466bb158497bf57af479574f6e0ecf
Vladislav Dektiarev <vladislav.dektiarev@soname.de>
2023-11-03 15:16:06 +0100

    Project Skeleton (#1)
    * Add default project skeleton
    
    * Add sample bucket and event bus
    
    * Add sample lambda function
    
    * Add deployment script and lambda declaration
    
    * Fix lambda source path
    
    * Move lambda to the upper directory
    
    * Fix lambda path to be OS agnostic
    
    * Update notification lambda max duration time
    
    * Add notification lambda role to get S3 objects and use SES
    
    * Add sh script for cdk deployment
    
    * Change and check stage env variable
    
    * Change eventbus name
    
    * Add setting files deployment mechanism
    
    * Add files to the subfolder
    
    * Reformat stack file
    
    * Remove unnecessary statement
    
    ---------
    
    Co-authored-by: Vladislav Dektiarev <vladislav.dektiarev@gmail.com>

---
+++ src/lambda/notification-lambda/index.py
@@ -0,0 +1,11 @@
+import os
+import json
+
+def generate_hello_world():
+    return "Hello World!"
+
+def lambda_handler(event, context):
+    helloworld = generate_hello_world()
+    return { 
+        'message' : helloworld
+    }
