9fcd3f6f1e5222f56b5972cef13a1d6530b33ae0
Vladislav Dektiarev <vladislav.dektiarev@gmail.com>
2023-11-09 19:43:07 +0100

    Fix formatting

--- infra_tooling_account/infra_tooling_account/infra_tooling_alerting_stack.py
+++ infra_tooling_account/infra_tooling_account/infra_tooling_alerting_stack.py
@@ -9,7 +9,7 @@ from aws_cdk import (
     aws_iam as iam,
     aws_s3_deployment as s3deploy,
     aws_sqs as sqs,
-    Duration
+    Duration,
 )
 from constructs import Construct
 import os
@@ -17,16 +17,15 @@ import json
 
 
 class InfraToolingAlertingStack(Stack):
-
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-
         stage_name = kwargs.pop("stage_name", None)
         project_name = kwargs.pop("project_name", None)
 
         super().__init__(scope, construct_id, **kwargs)
-        
-        input_timestream_database_arn = Fn.import_value(f"output-{project_name}-metrics-events-storage-arn-{stage_name}")
 
+        input_timestream_database_arn = Fn.import_value(
+            f"output-{project_name}-metrics-events-storage-arn-{stage_name}"
+        )
 
         # Settings S3 bucket
         settings_bucket_name = f"s3-{project_name}-settings-{stage_name}"
@@ -34,83 +33,97 @@ class InfraToolingAlertingStack(Stack):
             self,
             "salmonSettingsBucket",
             bucket_name=settings_bucket_name,
-            block_public_access=s3.BlockPublicAccess.BLOCK_ALL
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
         )
 
         # S3 settings files deployment
         s3deploy.BucketDeployment(
-            self, "salmonSettingsDeployment",
-            sources=[s3deploy.Source.asset('../config/settings')],
+            self,
+            "salmonSettingsDeployment",
+            sources=[s3deploy.Source.asset("../config/settings")],
             destination_bucket=settings_bucket,
-            destination_key_prefix='settings'
+            destination_key_prefix="settings",
         )
 
         # EventBridge Bus
         notification_bus = events.EventBus(
             self,
             "salmonNotificationsBus",
-            event_bus_name=f"eventbus-{project_name}-notification-{stage_name}"
+            event_bus_name=f"eventbus-{project_name}-notification-{stage_name}",
         )
-        
-        #EventBridge bus resource policy
+
+        # EventBridge bus resource policy
         general_settings_file_path = "../config/settings/general.json"
         with open(general_settings_file_path) as f:
             try:
                 general_config = json.load(f)
             except json.decoder.JSONDecodeError as e:
                 raise json.decoder.JSONDecodeError(
-                f"Error parsing JSON file {general_settings_file_path}", e.doc, e.pos
+                    f"Error parsing JSON file {general_settings_file_path}",
+                    e.doc,
+                    e.pos,
+                )
+
+        monitored_account_ids = [
+            account["AccountId"] for account in general_config["monitored_accounts"]
+        ]
+        monitored_principals = [
+            iam.AccountPrincipal(account_id) for account_id in monitored_account_ids
+        ]
+
+        notification_bus.add_to_resource_policy(
+            iam.PolicyStatement(
+                sid="AllowMonitoredAccountsPutEvents",
+                actions=["events:PutEvents"],
+                effect=iam.Effect.ALLOW,
+                resources=[notification_bus.event_bus_arn],
+                principals=monitored_principals,
             )
-        
-        monitored_account_ids = [account["AccountId"] for account in general_config["monitored_accounts"]]
-        monitored_principals = [iam.AccountPrincipal(account_id) for account_id in monitored_account_ids]
-            
-        notification_bus.add_to_resource_policy(iam.PolicyStatement(
-            sid="AllowMonitoredAccountsPutEvents",
-            actions=["events:PutEvents"],
-            effect=iam.Effect.ALLOW,
-            resources=[notification_bus.event_bus_arn],
-            principals=monitored_principals
-        ))
+        )
 
         # Notification Lambda Eventbridge rule
         notification_lambda_event_rule = events.Rule(
             self,
             "salmonNotificationLambdaEventRule",
-            rule_name=f"eventbusrule-{
-                project_name}-notification-lambda-{stage_name}",
+            rule_name=f"eventbusrule-{project_name}-notification-lambda-{stage_name}",
             event_bus=notification_bus,
-            event_pattern=events.EventPattern(source=events.Match.prefix("aws"))
+            event_pattern=events.EventPattern(source=events.Match.prefix("aws")),
         )
 
         # Notification Lambda Role
         notification_lambda_role = iam.Role(
-            self, "notificationLambdaRole",
-            assumed_by=iam.ServicePrincipal('lambda.amazonaws.com'),
-            role_name=f"role-{project_name}-notification-lambda-{stage_name}"
+            self,
+            "notificationLambdaRole",
+            assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"),
+            role_name=f"role-{project_name}-notification-lambda-{stage_name}",
         )
 
-        notification_lambda_role.add_to_policy(iam.PolicyStatement(
-            actions=["s3:GetObject"],
-            effect=iam.Effect.ALLOW,
-            resources=[settings_bucket.bucket_arn],
-        ))
+        notification_lambda_role.add_to_policy(
+            iam.PolicyStatement(
+                actions=["s3:GetObject"],
+                effect=iam.Effect.ALLOW,
+                resources=[settings_bucket.bucket_arn],
+            )
+        )
 
-        notification_lambda_role.add_to_policy(iam.PolicyStatement(
-            actions=["ses:SendEmail", "ses:SendRawEmail"],
-            effect=iam.Effect.ALLOW,
-            resources=["*"]
-        ))
-        
-        notification_lambda_role.add_to_policy(iam.PolicyStatement(
-            actions=["timestream:*"],
-            effect=iam.Effect.ALLOW,
-            resources=[input_timestream_database_arn]
-        ))
+        notification_lambda_role.add_to_policy(
+            iam.PolicyStatement(
+                actions=["ses:SendEmail", "ses:SendRawEmail"],
+                effect=iam.Effect.ALLOW,
+                resources=["*"],
+            )
+        )
+
+        notification_lambda_role.add_to_policy(
+            iam.PolicyStatement(
+                actions=["timestream:*"],
+                effect=iam.Effect.ALLOW,
+                resources=[input_timestream_database_arn],
+            )
+        )
 
         # Notification Lambda
-        notification_lambda_path = os.path.join(
-            '../src/', 'lambda/notification-lambda')
+        notification_lambda_path = os.path.join("../src/", "lambda/notification-lambda")
         notification_lambda = lambda_.Function(
             self,
             "salmonNotificationLambda",
@@ -119,22 +132,22 @@ class InfraToolingAlertingStack(Stack):
             handler="index.lambda_handler",
             timeout=Duration.seconds(30),
             runtime=lambda_.Runtime.PYTHON_3_11,
-            environment={'SETTINGS_S3_BUCKET_NAME': settings_bucket_name},
-            role=notification_lambda_role
+            environment={"SETTINGS_S3_BUCKET_NAME": settings_bucket_name},
+            role=notification_lambda_role,
         )
 
         # Notification Lambda EventBridge Rule Target
         notification_event_dlq = sqs.Queue(
             self,
             "notificationEventDlq",
-            queue_name=f"queue-{project_name}-notification-dlq-{stage_name}"
+            queue_name=f"queue-{project_name}-notification-dlq-{stage_name}",
         )
 
         notification_lambda_event_rule.add_target(
             targets.LambdaFunction(
                 notification_lambda,
                 dead_letter_queue=notification_event_dlq,
-                retry_attempts=3
+                retry_attempts=3,
             )
         )
 
