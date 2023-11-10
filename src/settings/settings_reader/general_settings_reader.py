6053375e4238f6a38e865e7030b52686f14305ac
Vladislav Dektiarev <vladislav.dektiarev@gmail.com>
2023-11-10 13:45:51 +0100

    Add support for tooling environment in the reader

--- src/settings/settings_reader/general_settings_reader.py
+++ src/settings/settings_reader/general_settings_reader.py
@@ -7,11 +7,13 @@ class GeneralSettingsReader(SettingsReader):
     This class extends the SettingsReader to provide access to General settings from a JSON file.
 
     Attributes:
+        tooling_environment (dict): Dictionary of the tooling environment.
         monitored_environments (dict): Dictionary of monitored environments.
         delivery_methods (dict): Dictionary of delivery methods.
 
     Methods:
-        get_monitored_environments: Retrieves the monitored accounts from the General settings.
+        get_tooling_environment: Retrieves the tooling environment from the General settings.
+        get_monitored_environments: Retrieves the monitored environments from the General settings.
         get_delivery_methods: Retrieves the delivery methods from the General settings.
 
     """
@@ -24,8 +26,17 @@ class GeneralSettingsReader(SettingsReader):
             settings_data (str): The content of the General settings file in JSON format.
         """
         super().__init__(settings_file_name, settings_data)
+        self.tooling_environment = self.get_setting("tooling_environment")
         self.monitored_environments = self.get_setting("monitored_environments")
         self.delivery_methods = self.get_setting("delivery_methods")
+
+    def get_tooling_environment(self) -> dict:
+        """Retrieves the tooling environment from the General settings.
+
+        Returns:
+            dict: Dictionary of tooling environment.
+        """
+        return self.tooling_environment
 
     def get_monitored_environments(self) -> dict:
         """Retrieves the monitored environments from the General settings.
--- tests/src/settings/settings_reader/test_general_settings_reader.py
+++ tests/src/settings/settings_reader/test_general_settings_reader.py
@@ -7,6 +7,7 @@ class TestGeneralSettingsReader:
     def general_settings_data(self):
         return """
         {
+            "tooling_environment": {"name": "account1"},
             "monitored_environments": [{"name": "account1"}, {"name": "account2"}],
             "delivery_methods": [{"name": "method1"}, {"name": "method2"}]
         }
@@ -15,6 +16,10 @@ class TestGeneralSettingsReader:
     @pytest.fixture(scope="class")
     def general_settings_reader(self, general_settings_data):
         return GeneralSettingsReader("general.json", general_settings_data)
+
+    def test_get_tooling_environment(self, general_settings_reader):
+        tooling_environment = general_settings_reader.get_tooling_environment()
+        assert tooling_environment == {"name": "account1"}
 
     def test_get_monitored_environments(self, general_settings_reader):
         monitored_environments = general_settings_reader.get_monitored_environments()
