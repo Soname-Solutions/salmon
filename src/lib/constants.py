5c69e8266d8a79aff12cbd6d90cb4f288e0fcfc4
Natallia Alkhimovich <alhimovichn@gmail.com>
2023-11-21 23:55:24 +0100

    fix: Removed not used constants.

--- src/lib/constants.py
+++ src/lib/constants.py
@@ -2,9 +2,3 @@ class SettingFileNames:
     GENERAL_FILE_NAME = "general.json"
     MONITORING_GROUPS_FILE_NAME = "monitoring_groups.json"
     RECIPIENTS_FILE_NAME = "recipients.json"
-
-
-class NotificationServiceConfig:
-    SES_SENDER = "my_email@soname.de"
-    ALERT_HEADER = "SDM [DEV] SAP Lean Glue Failure"
-    SMTP_SENDER = "my_email@soname.de"
