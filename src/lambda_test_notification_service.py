21145271508c06836e1653b58e1071870ada78ce
Natallia Alkhimovich <alhimovichn@gmail.com>
2023-11-21 23:52:55 +0100

    after review: Got rid of contrants usage.

--- src/lambda_test_notification_service.py
+++ src/lambda_test_notification_service.py
@@ -1,25 +1,18 @@
 import json
 
-from constants import NotificationServiceConfig
-
 from notification_service.sender_provider import senders
 from notification_service.messages import Message
 
+
 # todo: the config will be read from settings config
 sender_config = {
-    "ses_sender": NotificationServiceConfig.SES_SENDER,
-    "smtp_sender": NotificationServiceConfig.SMTP_SENDER,
+    "ses_sender": "my_email@soname.de",
+    "smtp_sender": "my_email@soname.de",
     "smtp_server": "smtp.gmail.com",
     "smtp_port": 465,
-    "smtp_login": NotificationServiceConfig.SMTP_SENDER,
-    "smtp_password": "",
+    "smtp_login": "my_email@soname.de",
+    "smtp_password": "my_pass",
 }
-
-# todo: recipients should be got from settings
-recipients = [
-    "my_email@soname.de"
-    # , "vasilii_pupkin@salmon.com"
-]
 
 
 def lambda_handler(event, context):
@@ -37,10 +30,13 @@ def lambda_handler(event, context):
         raise KeyError("Delivery method is not set.")
 
     message = Message(
-        delivery_info.get("message"), NotificationServiceConfig.ALERT_HEADER
+        delivery_info.get("message"), delivery_info.get("message_subject")
     )
     sender = senders.get(
-        delivery_method, message=message, recipients=recipients, **sender_config
+        delivery_method,
+        message=message,
+        recipients=delivery_info.get("recipients"),
+        **sender_config
     )
 
     try:
@@ -55,6 +51,8 @@ if __name__ == "__main__":
     test_event = """
         {
             "delivery_method": "AWS_SES",
+            "message_subject": "SDM [DEV] SAP Lean Glue Failure",
+            "recipients": ["my_email@soname.de", "vasilii_pupkin@salmon.com"],
             "message": "<html><head><style>table, th, td { border: 1px solid black; margin: 2px; }</style></head><body><h1>Hi</h1><table><tr><td>Glue Job 1 failed!</td></tr></table></body></html>"
         }
         """
