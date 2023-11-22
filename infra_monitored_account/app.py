9a6da7f035a3ded5452fbbeadb2d4b58381b5d58
Vladislav Dektiarev <vladislav.dektiarev@gmail.com>
2023-11-22 20:34:49 +0100

    Move settings provision to CDK to a separate class

--- infra_monitored_account/app.py
+++ infra_monitored_account/app.py
@@ -7,49 +7,9 @@ import aws_cdk as cdk
 sys.path.append("../src")
 from infra_monitored_account.infra_monitored_stack import InfraMonitoredStack
 
-from lib.core import file_manager
-from lib.core.constants import SettingFileNames
-from lib.settings.settings_reader import (
-    GeneralSettingsReader,
-    MonitoringSettingsReader,
-    RecipientsSettingsReader,
+from lib.settings.cdk.local_file_system_settings_provider import (
+    LocalFileSystemSettingsProvider,
 )
-from lib.settings import settings_validator
-
-
-def read_settings():
-    settings_files_path = "../config/settings"
-    general_file = file_manager.read_file(
-        os.path.join(settings_files_path, SettingFileNames.GENERAL_FILE_NAME)
-    )
-    monitoring_groups_file = file_manager.read_file(
-        os.path.join(settings_files_path, SettingFileNames.MONITORING_GROUPS_FILE_NAME)
-    )
-    recipients_file = file_manager.read_file(
-        os.path.join(settings_files_path, SettingFileNames.RECIPIENTS_FILE_NAME)
-    )
-
-    general_settings_reader = GeneralSettingsReader(
-        SettingFileNames.GENERAL_FILE_NAME, general_file
-    )
-    monitoring_groups_settings_reader = MonitoringSettingsReader(
-        SettingFileNames.MONITORING_GROUPS_FILE_NAME, monitoring_groups_file
-    )
-    recipients_settings_reader = RecipientsSettingsReader(
-        SettingFileNames.RECIPIENTS_FILE_NAME, recipients_file
-    )
-
-    settings_validator.validate(
-        general_settings_reader,
-        monitoring_groups_settings_reader,
-        recipients_settings_reader,
-    )
-
-    return (
-        general_settings_reader,
-        monitoring_groups_settings_reader,
-        recipients_settings_reader,
-    )
 
 
 if "STAGE_NAME" in os.environ:
@@ -60,11 +20,8 @@ STAGE_NAME = os.environ["STAGE_NAME"]
 
 PROJECT_NAME = "salmon"
 
-(
-    general_settings_reader,
-    monitoring_groups_settings_reader,
-    recipients_settings_reader,
-) = read_settings()
+settings_provider = LocalFileSystemSettingsProvider("../config/settings")
+settings_provider.validate_settings()
 
 app = cdk.App()
 InfraMonitoredStack(
@@ -73,7 +30,7 @@ InfraMonitoredStack(
     tags={"project_name": PROJECT_NAME, "stage_name": STAGE_NAME},
     project_name=PROJECT_NAME,
     stage_name=STAGE_NAME,
-    general_settings_reader=general_settings_reader,
+    general_settings_reader=settings_provider.general_settings_reader,
 )
 
 app.synth()
--- infra_tooling_account/app.py
+++ infra_tooling_account/app.py
@@ -11,49 +11,9 @@ from infra_tooling_account.infra_tooling_monitoring_stack import (
     InfraToolingMonitoringStack,
 )
 
-from lib.core import file_manager
-from lib.core.constants import SettingFileNames
-from lib.settings.settings_reader import (
-    GeneralSettingsReader,
-    MonitoringSettingsReader,
-    RecipientsSettingsReader,
+from lib.settings.cdk.local_file_system_settings_provider import (
+    LocalFileSystemSettingsProvider,
 )
-from lib.settings import settings_validator
-
-
-def read_settings():
-    settings_files_path = "../config/settings"
-    general_file = file_manager.read_file(
-        os.path.join(settings_files_path, SettingFileNames.GENERAL_FILE_NAME)
-    )
-    monitoring_groups_file = file_manager.read_file(
-        os.path.join(settings_files_path, SettingFileNames.MONITORING_GROUPS_FILE_NAME)
-    )
-    recipients_file = file_manager.read_file(
-        os.path.join(settings_files_path, SettingFileNames.RECIPIENTS_FILE_NAME)
-    )
-
-    general_settings_reader = GeneralSettingsReader(
-        SettingFileNames.GENERAL_FILE_NAME, general_file
-    )
-    monitoring_groups_settings_reader = MonitoringSettingsReader(
-        SettingFileNames.MONITORING_GROUPS_FILE_NAME, monitoring_groups_file
-    )
-    recipients_settings_reader = RecipientsSettingsReader(
-        SettingFileNames.RECIPIENTS_FILE_NAME, recipients_file
-    )
-
-    settings_validator.validate(
-        general_settings_reader,
-        monitoring_groups_settings_reader,
-        recipients_settings_reader,
-    )
-
-    return (
-        general_settings_reader,
-        monitoring_groups_settings_reader,
-        recipients_settings_reader,
-    )
 
 
 if "STAGE_NAME" in os.environ:
@@ -66,11 +26,8 @@ PROJECT_NAME = "salmon"
 
 TAGS = {"project_name": PROJECT_NAME, "stage_name": STAGE_NAME}
 
-(
-    general_settings_reader,
-    monitoring_groups_settings_reader,
-    recipients_settings_reader,
-) = read_settings()
+settings_provider = LocalFileSystemSettingsProvider("../config/settings")
+settings_provider.validate_settings()
 
 app = cdk.App()
 common_stack = InfraToolingCommonStack(
@@ -86,7 +43,7 @@ alerting_stack = InfraToolingAlertingStack(
     tags=TAGS,
     stage_name=STAGE_NAME,
     project_name=PROJECT_NAME,
-    general_settings_reader=general_settings_reader,
+    general_settings_reader=settings_provider.general_settings_reader,
 )
 monitoring_stack = InfraToolingMonitoringStack(
     app,
@@ -94,7 +51,7 @@ monitoring_stack = InfraToolingMonitoringStack(
     tags=TAGS,
     stage_name=STAGE_NAME,
     project_name=PROJECT_NAME,
-    general_settings_reader=general_settings_reader,
+    general_settings_reader=settings_provider.general_settings_reader,
 )
 
 alerting_stack.add_dependency(common_stack)
