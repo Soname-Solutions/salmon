d7000c6bee1fa94fffe5f11aa44c30296525e22d
Vladislav Dektiarev <vladislav.dektiarev@gmail.com>
2023-11-30 12:54:39 +0100

    Retrieve state machine name from arn coming with event

--- src/lib/event_mapper/impl/step_functions_event_mapper.py
+++ src/lib/event_mapper/impl/step_functions_event_mapper.py
@@ -1,13 +1,18 @@
 from .general_aws_event_mapper import GeneralAwsEventMapper
 from ...settings import Settings
+import boto3
 
 
 class StepFunctionsEventMapper(GeneralAwsEventMapper):
     def __init__(self, settings: Settings):
         super().__init__(settings)
+        self.client = boto3.client("stepfunctions")
 
     def get_resource_name(self, event):
-        return event["detail"]["stateMachineArn"]
+        state_machine = self.client.describe_state_machine(
+            stateMachineArn=event["detail"]["stateMachineArn"]
+        )
+        return state_machine["name"]
 
     def get_message(self, event):
         message = {}
