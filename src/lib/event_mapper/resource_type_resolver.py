8dd60293a9c937d63f06d7b2d5da110e0fd64723
Vladislav Dektiarev <vladislav.dektiarev@gmail.com>
2024-01-12 11:28:54 +0100

    Unify approaches for resource type resolver

--- src/lib/event_mapper/resource_type_resolver.py
+++ src/lib/event_mapper/resource_type_resolver.py
@@ -9,7 +9,7 @@ class ResourceTypeResolver:
             "Data Catalog": types.GLUE_DATA_CATALOGS,
             "Crawler": types.GLUE_CRAWLERS,
         },
-        "aws.states": types.STEP_FUNCTIONS,
+        "aws.states": {"Execution": types.STEP_FUNCTIONS},
     }
 
     @staticmethod
@@ -17,15 +17,13 @@ class ResourceTypeResolver:
         source = event["source"]
         detail_type = event["detail-type"]
 
-        source_resource_type = ResourceTypeResolver._source_detail_type_map[source]
+        detail_keyword_resource_type_map = ResourceTypeResolver._source_detail_type_map[
+            source
+        ]
 
-        if isinstance(source_resource_type, str):
-            return source_resource_type
-
-        if isinstance(source_resource_type, dict):
-            for keyword, resource_type in source_resource_type.items():
-                if keyword in detail_type:
-                    return resource_type
+        for keyword, resource_type in detail_keyword_resource_type_map.items():
+            if keyword in detail_type:
+                return resource_type
 
         raise KeyError(
             f"No event mapper configured for event detail type: {detail_type}"
