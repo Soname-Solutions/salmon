58962f343effd22af13f9a053522838449051c9e
Anatolii Maslov <amaslov78@gmail.com>
2024-02-05 15:06:16 +0300

    * Fixed: str_utc_datetime_to_datetime were failing if input had ".000000" milliseconds part + Added pytests for this function

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,7 @@
-# TBD: for future common fixtures (if any)
+# conftest.py
+import sys
+from pathlib import Path
+
+# Append the directory above "src" to the PYTHONPATH
+# This allows tests to import from src.lib.core
+sys.path.append(str(Path(__file__).resolve().parent.parent))
