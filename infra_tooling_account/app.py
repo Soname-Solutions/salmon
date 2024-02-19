3ead3bc2869deaa91d6c1b0b5659178dbf5e0198
Aryna Yukhnevich <aryna.yukhnevich@soname.de>
2024-02-19 11:19:19 +0300

    check env variables  (#59)
    * check whether current account and region are aligned with the values provided in the config file
    
    * remove test imports
    
    * remove test imports

--- infra_tooling_account/app.py
+++ infra_tooling_account/app.py
@@ -20,14 +20,27 @@ from lib.settings.cdk import settings_validator
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+ENV_NAME = "tooling_environment"
+
 settings = Settings.from_file_path("../config/settings")
 settings_validator.validate(settings)
 
-account_id, region = settings.get_tooling_account_props()
+current_account = os.getenv("CDK_DEFAULT_ACCOUNT")
+current_region = os.getenv("CDK_DEFAULT_REGION")
+logging.info(
+    f"Target AWS account: {current_account} and AWS Region: {current_region} determined by the AWS CDK"
+)
+
+tooling_account_id, tooling_region = settings.get_tooling_account_props()
 env = {
-    "region": region,
-    "account": account_id,
+    "region": tooling_region,
+    "account": tooling_account_id,
 }
+settings_validator.validate_cdk_env_variables(
+    env_name=ENV_NAME,
+    cdk_env_variables={(current_account, current_region)},
+    config_values={(tooling_account_id, tooling_region)},
+)
 
 app = cdk.App()
 
