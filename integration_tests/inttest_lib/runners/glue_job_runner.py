6bef04359f5ea152d36e1ba1c148e93d9f698de5
Anatolii Maslov <amaslov78@gmail.com>
2024-08-05 16:43:30 +0300

    added base runner and glue job runner for integration tests

---
+++ integration_tests/inttest_lib/runners/glue_job_runner.py
@@ -0,0 +1,36 @@
+import boto3
+import time
+
+from inttest_lib.runners.base_resource_runner import BaseResourceRunner
+
+from lib.aws.glue_manager import GlueManager
+
+class GlueJobRunner(BaseResourceRunner):
+    def __init__(self, resource_names, region_name):
+        super().__init__(resource_names, region_name)
+        self.client = boto3.client('glue', region_name=region_name)
+        self.job_runs = {}
+
+    def initiate(self):
+        for job_name in self.resource_names:
+            response = self.client.start_job_run(JobName=job_name)
+            self.job_runs[job_name] = response['JobRunId']
+            print(f"Started Glue job {job_name} with run ID {response['JobRunId']}")
+
+    def await_completion(self, poll_interval=10):
+        while True:
+            all_completed = True
+            for job_name, run_id in self.job_runs.items():
+                response = self.client.get_job_run(JobName=job_name, RunId=run_id)
+                status = response['JobRun']['JobRunState']
+                print(f"Job {job_name} with run ID {run_id} is in state {status}")
+                if not GlueManager.is_job_final_state(status):
+#                if status not in ['SUCCEEDED', 'FAILED', 'STOPPED']:
+                    all_completed = False
+
+            if all_completed:
+                break
+
+            time.sleep(poll_interval)  # Wait for the specified poll interval before checking again
+
+        print("All Glue jobs have completed.")
---
+++ integration_tests/testing_stand_execution.py
@@ -0,0 +1,36 @@
+import argparse
+import os
+import sys
+
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+lib_path = os.path.join(project_root, 'src')
+sys.path.append(lib_path)
+
+from inttest_lib.runners.base_resource_runner import BaseResourceRunner
+from inttest_lib.runners.glue_job_runner import GlueJobRunner
+
+
+def main():
+    # 1. prepare
+    parser = argparse.ArgumentParser(description="Process some settings.")
+    parser.add_argument("--stage-name", required=True, type=str, help="stage-name")
+    parser.add_argument("--region", required=True, type=str, help="region")
+    args = parser.parse_args()
+
+    stage_name = args.stage_name
+    region = args.region
+
+    # 2. run testing stand resources
+    glue_job_names = [f"glue-salmon-pyjob-success-{stage_name}", f"glue-salmon-pyjob-fail-{stage_name}"]
+    runner = GlueJobRunner(resource_names = glue_job_names, region_name = region)
+
+    runner.initiate()
+
+    runner.await_completion( poll_interval = 5 )
+
+    # 3. execute extract-metrics-orch lambda (in async mode, so if failure - destination would work)
+
+
+
+if __name__ == "__main__":
+    main()
