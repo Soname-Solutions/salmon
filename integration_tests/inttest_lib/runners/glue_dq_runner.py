97719a35f21bb9dad955ded4ae1b6a8c0c0efe3b
Aryna Yukhnevich <aryna.yukhnevich@soname.de>
2024-08-23 18:30:29 +0400

    add comments

--- integration_tests/inttest_lib/runners/glue_dq_runner.py
+++ integration_tests/inttest_lib/runners/glue_dq_runner.py
@@ -1,5 +1,6 @@
 import boto3
 import time
+from datetime import datetime
 
 from inttest_lib.runners.base_resource_runner import BaseResourceRunner
 from lib.aws.aws_naming import AWSNaming
@@ -8,21 +9,30 @@ DQ_MEANING = "dq"
 
 
 class GlueDQRunner(BaseResourceRunner):
-    def __init__(self, resource_names, region_name, started_after):
+    def __init__(
+        self, resource_names, region_name, started_after, stack_obj_for_naming
+    ):
         super().__init__(resource_names, region_name)
         self.client = boto3.client("glue", region_name=region_name)
-        self.started_after = started_after
+        self.started_after = datetime.fromtimestamp(started_after).strftime(
+            "%Y-%m-%d %H:%M:%S.%f"
+        )
+        self.stack_obj_for_naming = stack_obj_for_naming
 
     def initiate(self):
         for ruleset__name in self.resource_names:
             response = self.client.start_data_quality_ruleset_evaluation_run(
                 DataSource={
                     "GlueTable": {
-                        "DatabaseName": AWSNaming.GlueDB(self, DQ_MEANING),
-                        "TableName": AWSNaming.GlueTable(self, DQ_MEANING),
+                        "DatabaseName": AWSNaming.GlueDB(
+                            self.stack_obj_for_naming, DQ_MEANING
+                        ),
+                        "TableName": AWSNaming.GlueTable(
+                            self.stack_obj_for_naming, DQ_MEANING
+                        ),
                     }
                 },
-                Role=AWSNaming.IAMRole(self, DQ_MEANING),
+                Role=AWSNaming.IAMRole(self.stack_obj_for_naming, DQ_MEANING),
                 RulesetNames=[ruleset__name],
             )
             print(
@@ -42,7 +52,7 @@ class GlueDQRunner(BaseResourceRunner):
                 result["RulesetName"] for result in response_2.get("Results", [])
             ]
 
-            # check if the all rulesetes monitored are finished
+            # check if the results of all monitored rulesetes are returned
             incomplete_rulesets = [
                 name for name in self.resource_names if name not in ruleset_names
             ]
@@ -57,4 +67,4 @@ class GlueDQRunner(BaseResourceRunner):
                 poll_interval
             )  # Wait for the specified poll interval before checking again
 
-        print("All Glue Rulesets have completed.")
+        print("All Glue DQ Rulesets have completed.")
