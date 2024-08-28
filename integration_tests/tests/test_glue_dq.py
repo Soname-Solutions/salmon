c862532c923ceaef8475c04c8c1ce0e8facb2605
Anatolii Maslov <amaslov78@gmail.com>
2024-08-28 04:55:27 +0300

    added tests for Glue DQ

--- integration_tests/tests/test_glue_dq.py
+++ integration_tests/tests/test_glue_dq.py
@@ -5,18 +5,21 @@ from inttest_lib.dynamo_db_reader import IntegrationTestMessage
 from inttest_lib.message_checker import MessagesChecker
 
 from lib.aws.aws_naming import AWSNaming
+from lib.aws.glue_manager import GlueManager
 from lib.core.constants import SettingConfigResourceTypes
 from lib.aws.timestream_manager import TimeStreamQueryRunner
 
 @pytest.fixture(scope='session')
-def glue_execution_timestream_metrics_summary(region, start_epochtimemsec, stack_obj_for_naming):
+def glue_dq_execution_timestream_metrics_summary(region, start_epochtimemsec, stack_obj_for_naming):
     """
         Collects summary from relevant timestream table (records only since the test started are included).
         Result is dict: e.g. {'executions': '2', 'succeeded': '1', 'failed': '1'}
+
+        Sic! Counts only independent rulesets (running not inside GlueJobs)
     """
     DB_NAME = AWSNaming.TimestreamDB(stack_obj_for_naming, "metrics-events-storage")
     TABLE_NAME = AWSNaming.TimestreamMetricsTable(
-        stack_obj_for_naming, SettingConfigResourceTypes.GLUE_JOBS
+        stack_obj_for_naming, SettingConfigResourceTypes.GLUE_DATA_QUALITY
     )
 
     client = boto3.client("timestream-query", region_name=region)
@@ -37,21 +40,22 @@ def test_alerts(test_results_messages):
     """    
     msqchk = MessagesChecker(test_results_messages)
 
-    cnt_glue_error_messages = len(msqchk.subject_contains_all(["glue_jobs :", "FAILED"]))
-    cnt_glue_all_messages = len(msqchk.subject_contains_all(["glue_jobs :"]))
+    cnt_glue_error_messages = len(msqchk.subject_contains_all(["glue_data_quality :", "FAILED"]))
+    cnt_glue_all_messages = len(msqchk.subject_contains_all(["glue_data_quality :"]))
     
-    assert cnt_glue_error_messages == 1, "There should be exactly one glue job error message"
-    assert cnt_glue_all_messages == 1, "There should be exactly one glue job message"
+    # 1 - alert for ruleset inside Glue Job, 1 - for independent ruleset
+    assert cnt_glue_error_messages == 2, "There should be exactly two glue dq error messages"
+    assert cnt_glue_all_messages == 2, "There should be exactly two glue dq messages"
 
-def test_timestream_records(glue_execution_timestream_metrics_summary):
+def test_timestream_records(glue_dq_execution_timestream_metrics_summary):
     """
         Checking if timestream table is populated with correct data
     """
-    executions = glue_execution_timestream_metrics_summary.get('executions',0)
-    succeeded = glue_execution_timestream_metrics_summary.get('succeeded',0)
-    failed = glue_execution_timestream_metrics_summary.get('failed',0)
+    executions = glue_dq_execution_timestream_metrics_summary.get('executions',0)
+    succeeded = glue_dq_execution_timestream_metrics_summary.get('succeeded',0)
+    failed = glue_dq_execution_timestream_metrics_summary.get('failed',0)
 
-    assert executions == '2', "There should be exactly 2 executions. One for each glue job."
+    assert executions == '2', "There should be exactly 2 executions. One for each INDEPENDENT glue dq ruleset."
     assert succeeded == '1', "There should be exactly 1 successful execution."
     assert failed == '1', "There should be exactly 1 failed execution."
 
@@ -66,9 +70,9 @@ def test_digest_message(test_results_messages, config_reader, stack_obj_for_nami
     message_body = digest_messages[0].MessageBody
     
     # checking if there are mentions of testing stand glue jobs in the digest
-    glue_job_names = config_reader.get_names_by_resource_type(SettingConfigResourceTypes.GLUE_JOBS, stack_obj_for_naming)
+    glue_ruleset_names, _ = config_reader.get_glue_dq_names(GlueManager.DQ_Catalog_Context_Type, stack_obj_for_naming)
 
-    assert len(glue_job_names) > 0, "There should be glue jobs in testing scope"
+    assert len(glue_ruleset_names) > 0, "There should be glue rulesets in testing scope"
 
-    for glue_job_name in glue_job_names:
-        assert glue_job_name in message_body, f"There should be mention of {glue_job_name} glue job"
+    for glue_ruleset_name in glue_ruleset_names:
+        assert glue_ruleset_name in message_body, f"There should be mention of {glue_ruleset_name} glue ruleset"
