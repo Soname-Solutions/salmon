ddea2efe00e335116eeaf847db8df391a9d61f64
Anatolii Maslov <amaslov78@gmail.com>
2024-09-05 19:10:26 +0300

    + added StepFunctions in testing_stand_executor + added tests for StepFunctions

--- integration_tests/tests/test_step_functions.py
+++ integration_tests/tests/test_step_functions.py
@@ -8,21 +8,20 @@ from lib.core.constants import SettingConfigResourceTypes
 from lib.aws.timestream_manager import TimeStreamQueryRunner
 
 @pytest.fixture(scope='session')
-def gluewf_execution_timestream_metrics_summary(region, start_epochtimemsec, stack_obj_for_naming):
+def sfn_execution_timestream_metrics_summary(region, start_epochtimemsec, stack_obj_for_naming):
     """
         Collects summary from relevant timestream table (records only since the test started are included).
-        Result is dict: e.g. {'executions': '2', 'succeeded': '1', 'failed': '1', 'actions_failed': '1'}
+        Result is dict: e.g. {'executions': '2', 'succeeded': '1', 'failed': '1'}
     """
     DB_NAME = AWSNaming.TimestreamDB(stack_obj_for_naming, "metrics-events-storage")
     TABLE_NAME = AWSNaming.TimestreamMetricsTable(
-        stack_obj_for_naming, SettingConfigResourceTypes.GLUE_WORKFLOWS
+        stack_obj_for_naming, SettingConfigResourceTypes.STEP_FUNCTIONS
     )
 
     client = boto3.client("timestream-query", region_name=region)
     query_runner = TimeStreamQueryRunner(client)
 
     query = f"""SELECT sum(execution) as executions, sum(succeeded) as succeeded, sum(failed) as failed
-                     , sum(actions_failed) as actions_failed
                 FROM "{DB_NAME}"."{TABLE_NAME}"
                 WHERE time > from_milliseconds({start_epochtimemsec})
     """   
@@ -37,25 +36,23 @@ def test_alerts(test_results_messages):
     """    
     msqchk = MessagesChecker(test_results_messages)
 
-    cnt_glue_error_messages = len(msqchk.subject_contains_all(["glue_workflows :", "FAILED"]))
-    cnt_glue_all_messages = len(msqchk.subject_contains_all(["glue_workflows :"]))
+    cnt_glue_error_messages = len(msqchk.subject_contains_all(["step_functions :", "FAILED"]))
+    cnt_glue_all_messages = len(msqchk.subject_contains_all(["step_functions :"]))
     
-    assert cnt_glue_error_messages == 1, "There should be exactly one glue workflow error message"
-    assert cnt_glue_all_messages == 1, "There should be exactly one glue workflow message"
+    assert cnt_glue_error_messages == 1, "There should be exactly one step function error message"
+    assert cnt_glue_all_messages == 1, "There should be exactly one step function message"
 
-def test_timestream_records(gluewf_execution_timestream_metrics_summary):
+def test_timestream_records(sfn_execution_timestream_metrics_summary):
     """
         Checking if timestream table is populated with correct data
     """
-    executions = gluewf_execution_timestream_metrics_summary.get('executions',0)
-    succeeded = gluewf_execution_timestream_metrics_summary.get('succeeded',0)
-    failed = gluewf_execution_timestream_metrics_summary.get('failed',0)
-    actions_failed = gluewf_execution_timestream_metrics_summary.get('actions_failed',0)
+    executions = sfn_execution_timestream_metrics_summary.get('executions',0)
+    succeeded = sfn_execution_timestream_metrics_summary.get('succeeded',0)
+    failed = sfn_execution_timestream_metrics_summary.get('failed',0)
 
-    assert executions == '2', "There should be exactly 2 executions. One for each glue workflow."
+    assert executions == '2', "There should be exactly 2 executions. One for each step function."
     assert succeeded == '1', "There should be exactly 1 successful execution."
     assert failed == '1', "There should be exactly 1 failed execution."
-    assert actions_failed == '1', "There should be exactly 1 failed action."
 
 def test_digest_message(test_results_messages, config_reader, stack_obj_for_naming):
     """
@@ -67,10 +64,10 @@ def test_digest_message(test_results_messages, config_reader, stack_obj_for_nami
 
     message_body = digest_messages[0].MessageBody
     
-    # checking if there are mentions of testing stand glue workflows in the digest
-    glue_workflow_names = config_reader.get_names_by_resource_type(SettingConfigResourceTypes.GLUE_WORKFLOWS, stack_obj_for_naming)
+    # checking if there are mentions of testing stand step functions in the digest
+    step_function_names = config_reader.get_names_by_resource_type(SettingConfigResourceTypes.STEP_FUNCTIONS, stack_obj_for_naming)
 
-    assert len(glue_workflow_names) > 0, "There should be glue workflows in testing scope"
+    assert len(step_function_names) > 0, "There should be step functions in testing scope"
 
-    for glue_workflow_name in glue_workflow_names:
-        assert glue_workflow_name in message_body, f"There should be mention of {glue_workflow_name} glue workflow"
+    for step_function_name in step_function_names:
+        assert step_function_name in message_body, f"There should be mention of {step_function_name} step function"
