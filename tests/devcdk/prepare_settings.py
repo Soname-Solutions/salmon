a9a5462b76f304940fa158466de74401d2c81557
Aryna Yukhnevich <aryna.yukhnevich@soname.de>
2024-09-10 14:32:54 +0400

    add grafana stack to the deployment tests

--- tests/devcdk/prepare_settings.py
+++ tests/devcdk/prepare_settings.py
@@ -6,6 +6,12 @@ import os
 # The script can be modified for any specific set of config files
 
 
+class PrepareSettingsException(Exception):
+    """Exception raised for errors encountered while preparing test settings."""
+
+    pass
+
+
 def get_aws_account_id():
     session = boto3.Session()
     sts_client = session.client("sts")
@@ -14,15 +20,48 @@ def get_aws_account_id():
     return account_id
 
 
+def get_vpc_id():
+    """Required for the deployment of Grafana stack"""
+    session = boto3.Session()
+    ec2_client = session.client("ec2")
+    response = ec2_client.describe_vpcs(
+        Filters=[{"Name": "isDefault", "Values": ["true"]}]
+    )
+
+    try:
+        vpc_id = response["Vpcs"][0]["VpcId"]
+        return vpc_id
+    except Exception as ex:
+        raise PrepareSettingsException(f"Error while getting default VPC ID: {str(ex)}")
+
+
+def get_security_group_id():
+    """Required for the deployment of Grafana stack"""
+    session = boto3.Session()
+    ec2_client = session.client("ec2")
+    response = ec2_client.describe_security_groups(GroupNames=["default"])
+
+    try:
+        security_group_id = response["SecurityGroups"][0]["GroupId"]
+        return security_group_id
+    except Exception as ex:
+        raise PrepareSettingsException(
+            f"Error while getting default security group ID: {str(ex)}"
+        )
+
+
 def read_replacements_file(file_path):
     with open(file_path, "r") as file:
         data = json.load(file)
     return data
 
 
-def update_replacements_file(file_path, aws_account_id):
+def update_replacements_file(file_path, aws_account_id, vpc_id, security_group_id):
     data = read_replacements_file(file_path)
     data["<<main_account_id>>"] = aws_account_id
+    data["<<grafana_vpc_id>>"] = vpc_id
+    data["<<grafana_security_group_id>>"] = security_group_id
+
     with open(file_path, "w") as file:
         json.dump(data, file, indent=4)
 
@@ -32,7 +71,9 @@ def main():
     file_path = os.path.join(script_dir, "settings", "replacements.json")
 
     aws_account_id = get_aws_account_id()
-    update_replacements_file(file_path, aws_account_id)
+    vpc_id = get_vpc_id()
+    security_group_id = get_security_group_id()
+    update_replacements_file(file_path, aws_account_id, vpc_id, security_group_id)
     print(f"Updated replacements.json with AWS Account ID: {aws_account_id}")
 
 
