0ffb4d7860e3504a008182ec75fd3694c42ea5a8
Aryna Yukhnevich <aryna.yukhnevich@soname.de>
2024-09-20 15:24:54 +0400

    enable lambda functions tests

--- integration_tests/tests/test_lambda_functions.py
+++ integration_tests/tests/test_lambda_functions.py
@@ -7,11 +7,14 @@ from lib.aws.aws_naming import AWSNaming
 from lib.core.constants import SettingConfigResourceTypes
 from lib.aws.timestream_manager import TimeStreamQueryRunner
 
-@pytest.fixture(scope='session')
-def lambda_execution_timestream_metrics_summary(region, start_epochtimemsec, stack_obj_for_naming):
+
+@pytest.fixture(scope="session")
+def lambda_execution_timestream_metrics_summary(
+    region, start_epochtimemsec, stack_obj_for_naming
+):
     """
-        Collects count of lambda executions
-        Result is dict: e.g. {'executions': '2'}
+    Collects count of lambda executions
+    Result is dict: e.g. {'executions': '2'}
     """
     DB_NAME = AWSNaming.TimestreamDB(stack_obj_for_naming, "metrics-events-storage")
     TABLE_NAME = AWSNaming.TimestreamMetricsTable(
@@ -24,16 +27,21 @@ def lambda_execution_timestream_metrics_summary(region, start_epochtimemsec, sta
     query = f"""SELECT count(*) as executions
                 FROM "{DB_NAME}"."{TABLE_NAME}"
                 WHERE measure_name = 'execution' AND time > from_milliseconds({start_epochtimemsec})
-    """   
+    """
     result = query_runner.execute_query(query=query)
 
-    return result[0] # returning the first record (it's only 1 record in resultset by query design)
+    return result[
+        0
+    ]  # returning the first record (it's only 1 record in resultset by query design)
 
-@pytest.fixture(scope='session')
-def lambda_error_timestream_metrics_summary(region, start_epochtimemsec, stack_obj_for_naming):
+
+@pytest.fixture(scope="session")
+def lambda_error_timestream_metrics_summary(
+    region, start_epochtimemsec, stack_obj_for_naming
+):
     """
-        Collects count of lambda errors
-        Result is dict: e.g. {'errors': '2'}
+    Collects count of lambda errors
+    Result is dict: e.g. {'errors': '2'}
     """
     DB_NAME = AWSNaming.TimestreamDB(stack_obj_for_naming, "metrics-events-storage")
     TABLE_NAME = AWSNaming.TimestreamMetricsTable(
@@ -46,51 +54,71 @@ def lambda_error_timestream_metrics_summary(region, start_epochtimemsec, stack_o
     query = f"""SELECT count(*) as errors
                 FROM "{DB_NAME}"."{TABLE_NAME}"
                 WHERE measure_name = 'error' AND time > from_milliseconds({start_epochtimemsec})
-    """   
+    """
     result = query_runner.execute_query(query=query)
 
-    return result[0] # returning the first record (it's only 1 record in resultset by query design)
+    return result[
+        0
+    ]  # returning the first record (it's only 1 record in resultset by query design)
+
 
 #######################################################################################################################
-@pytest.mark.skip
 def test_alerts(test_results_messages):
     """
-        Checking if correct notifications were sent
-    """    
+    Checking if correct notifications were sent
+    """
     msqchk = MessagesChecker(test_results_messages)
 
-    cnt_lambda_error_messages = len(msqchk.subject_contains_all(["lambda_functions :", "FAILED"]))
+    cnt_lambda_error_messages = len(
+        msqchk.subject_contains_all(["lambda_functions :", "FAILED"])
+    )
     cnt_lambda_all_messages = len(msqchk.subject_contains_all(["lambda_functions :"]))
-    
-    assert cnt_lambda_error_messages == 1, "There should be exactly one lambda function error message"
-    assert cnt_lambda_all_messages == 1, "There should be exactly one lambda function message"
 
-@pytest.mark.skip
-def test_timestream_records(lambda_execution_timestream_metrics_summary, lambda_error_timestream_metrics_summary):
+    assert (
+        cnt_lambda_error_messages == 1
+    ), "There should be exactly one lambda function error message"
+    assert (
+        cnt_lambda_all_messages == 1
+    ), "There should be exactly one lambda function message"
+
+
+def test_timestream_records(
+    lambda_execution_timestream_metrics_summary, lambda_error_timestream_metrics_summary
+):
     """
-        Checking if timestream table is populated with correct data
+    Checking if timestream table is populated with correct data
     """
-    executions = lambda_execution_timestream_metrics_summary.get('executions',0)
-    errors = lambda_error_timestream_metrics_summary.get('errors',0)
+    executions = lambda_execution_timestream_metrics_summary.get("executions", 0)
+    errors = lambda_error_timestream_metrics_summary.get("errors", 0)
 
-    assert executions == '2', "There should be exactly 2 executions. One for each lambda function."
-    assert errors == '1', "There should be exactly 1 failed execution."
+    assert (
+        executions == "2"
+    ), "There should be exactly 2 executions. One for each lambda function."
+    assert errors == "1", "There should be exactly 1 failed execution."
 
-@pytest.mark.skip
+
 def test_digest_message(test_results_messages, config_reader, stack_obj_for_naming):
     """
-        Checking if digest contains expected information
-    """    
+    Checking if digest contains expected information
+    """
     msqchk = MessagesChecker(test_results_messages)
 
-    digest_messages: list[IntegrationTestMessage] = msqchk.subject_contains_all(["Digest Report"])
+    digest_messages: list[IntegrationTestMessage] = msqchk.subject_contains_all(
+        ["Digest Report"]
+    )
 
     message_body = digest_messages[0].MessageBody
-    
-    # checking if there are mentions of testing stand glue jobs in the digest
-    lambda_function_names = config_reader.get_names_by_resource_type(SettingConfigResourceTypes.LAMBDA_FUNCTIONS, stack_obj_for_naming)
 
-    assert len(lambda_function_names) > 0, "There should be lambda functions in testing scope"
+    # checking if there are mentions of testing stand glue jobs in the digest
+    lambda_function_names = config_reader.get_names_by_resource_type(
+        SettingConfigResourceTypes.LAMBDA_FUNCTIONS, stack_obj_for_naming
+    )
+
+    assert (
+        len(lambda_function_names) > 0
+    ), "There should be lambda functions in testing scope"
 
     for lambda_function_name in lambda_function_names:
-        assert lambda_function_name in message_body, f"There should be mention of {lambda_function_name} lambda function"
+        assert (
+            lambda_function_name in message_body
+        ), f"There should be mention of {lambda_function_name} lambda function"
