f3723ab213a5a546033429aba370a972d5ece850
Anatolii Maslov <amaslov78@gmail.com>
2024-10-01 15:45:28 +0300

    added tests for EMRManager is_final_state

--- tests/src/lib/aws/test_emr_manager.py
+++ tests/src/lib/aws/test_emr_manager.py
@@ -1,3 +1,5 @@
+import pytest
+
 from datetime import datetime, timedelta, timezone
 
 from lib.aws.emr_manager import EMRManager, EMRJobRunData, ResourceUtilization
@@ -6,7 +8,7 @@ cur_time = datetime.now().astimezone(timezone.utc) - timedelta(minutes=10)
 
 JOB_RUN_PENDING = EMRJobRunData(
     applicationId="app1",
-    jobRunId="jobrun1",
+    jobRunId="jobrun_pending1",
     name="job1",
     createdAt=cur_time,
     updatedAt=cur_time,
@@ -15,7 +17,7 @@ JOB_RUN_PENDING = EMRJobRunData(
 
 JOB_RUN_COMPLETED = EMRJobRunData(
     applicationId="app1",
-    jobRunId="jobrun1",
+    jobRunId="jobrun_completed1",
     name="job1",
     createdAt=cur_time,
     updatedAt=cur_time,
@@ -30,9 +32,23 @@ JOB_RUN_COMPLETED = EMRJobRunData(
 
 JOB_RUN_COMPLETED_NO_RESOURCE_UTILIZATION = EMRJobRunData(
     applicationId="app1",
-    jobRunId="jobrun1",
+    jobRunId="jobrun_completed_no_resource_utilization1",
     name="job1",
     createdAt=cur_time,
     updatedAt=cur_time,
     state="SUCCESS",
 )
+
+
+@pytest.mark.parametrize(
+    ("job_run_data, is_final_state"),
+    [
+        (JOB_RUN_PENDING, False),
+        (JOB_RUN_COMPLETED, True),
+        (JOB_RUN_COMPLETED_NO_RESOURCE_UTILIZATION, False),
+    ],
+)
+def test_is_final_state(job_run_data: EMRJobRunData, is_final_state: bool):
+    assert (
+        job_run_data.is_final_state == is_final_state
+    ), f"For jobrun {job_run_data.jobRunId}, is_final_state should be {is_final_state}"
