e0e272355fde1dda92fe8a76e975ba7dbbff8865
Anatolii Maslov <amaslov78@gmail.com>
2024-10-15 16:09:09 +0300

    removed commented code in lambda_manager

--- src/lib/aws/lambda_manager.py
+++ src/lib/aws/lambda_manager.py
@@ -101,15 +101,6 @@ class LambdaManager:
             boto3.client("lambda") if lambda_client is None else lambda_client
         )
 
-    # def get_all_names(self, **kwargs):
-    #     try:
-    #         response = self.lambda_client.list_functions()
-    #         return [res["FunctionName"] for res in response.get("Functions")]
-
-    #     except Exception as e:
-    #         error_message = f"Error getting list of lambda functions : {e}"
-    #         raise LambdaManagerException(error_message)
-
     def get_all_names(self, **kwargs):
         try:
             paginator = self.lambda_client.get_paginator("list_functions")
