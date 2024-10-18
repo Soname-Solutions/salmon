a5f4028d14871974e7efce3eb722ffd04f6a317c
Aryna Yukhnevich <aryna.yukhnevich@soname.de>
2024-10-18 14:39:06 +0400

    udapte ts-lambda-mix-3.py

--- integration_tests/src_testing_stand/lambda_functions/ts-lambda-mix-3.py
+++ integration_tests/src_testing_stand/lambda_functions/ts-lambda-mix-3.py
@@ -3,27 +3,27 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-failure_count = 0
-last_request_id = None
+lambda_state = {"failure_count": 0, "last_request_id": None}
 
 
 def lambda_handler(event, context):
     current_request_id = context.aws_request_id
 
-    global failure_count, last_request_id
-    if last_request_id != current_request_id:
+    if lambda_state["last_request_id"] != current_request_id:
         # Reset failure count for the new request ID
-        failure_count = 0
-        last_request_id = current_request_id
+        lambda_state["failure_count"] = 0
+        lambda_state["last_request_id"] = current_request_id
 
-    if failure_count == 0:
+    if lambda_state["failure_count"] == 0:
         logger.info("Started first unsuccessful lambda execution - ts-lambda-mix-3")
-        failure_count += 1
+        lambda_state["failure_count"] += 1
         raise Exception("Intentional failure on first run - ts-lambda-mix-3")
-    elif failure_count == 1:
+
+    elif lambda_state["failure_count"] == 1:
         logger.info("Started second unsuccessful lambda execution - ts-lambda-mix-3")
-        failure_count += 1
+        lambda_state["failure_count"] += 1
         raise Exception("Intentional failure on second run - ts-lambda-mix-3")
+
     else:
         logger.info("Started third successful lambda execution - ts-lambda-mix-3")
         return {
