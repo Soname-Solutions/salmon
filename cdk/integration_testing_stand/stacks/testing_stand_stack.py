07e484fc60c5383809b5a8d3d3da7baffae32fa2
Aryna Yukhnevich <aryna.yukhnevich@soname.de>
2024-11-12 18:34:12 +0400

    add tag to sns topic

--- cdk/integration_testing_stand/stacks/testing_stand_stack.py
+++ cdk/integration_testing_stand/stacks/testing_stand_stack.py
@@ -103,6 +103,8 @@ class TestingStandStack(Stack):
             self, "TargetSNSTopic", display_name=topic_name, topic_name=topic_name
         )
 
+        Tags.of(target_topic).add("salmon", "project")
+
         # Create target DynamoDB table
         target_table_name = AWSNaming.DynamoDBTable(self, TARGET_MEANING)
         messages_table = dynamodb.Table(
