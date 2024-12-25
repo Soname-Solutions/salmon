a7b68fe0becfe0f4c63bd658885926686b78130c
Anatolii Maslov <amaslov78@gmail.com>
2024-12-26 02:15:36 +0300

    clean up of test_lambda_metrics_extractor

--- tests/unit-tests/src/test_lambda_extract_metrics.py
+++ tests/unit-tests/src/test_lambda_extract_metrics.py
@@ -1,6 +1,5 @@
 from datetime import datetime, timezone
 import pytest
-import os
 
 from lib.core.datetime_utils import str_utc_datetime_to_datetime
 from lambda_extract_metrics import (
@@ -10,16 +9,18 @@ from lambda_extract_metrics import (
     collect_glue_data_quality_result_ids,
     get_since_time_for_individual_resource,
 )
-from unittest.mock import patch, call, MagicMock, ANY
+from unittest.mock import patch, call, MagicMock
 from lib.core.constants import SettingConfigs, SettingConfigResourceTypes as types
 
-# uncomment this to see lambda's logging output
-import logging
+# # uncomment this to see lambda's logging output
+# import logging
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-handler = logging.StreamHandler()
-logger.addHandler(handler)
+# logger = logging.getLogger()
+# logger.setLevel(logging.INFO)
+# handler = logging.StreamHandler()
+# logger.addHandler(handler)
+
+#########################################################################################
 
 LAST_UPDATE_TIMES_SAMPLE = {
     "glue_jobs": [
@@ -49,142 +50,8 @@ LAST_UPDATE_TIMES_SAMPLE = {
         },
     ],
 }
-TEST_DQ_RESULT_IDS = ["result-id-1", "result-id-2"]
+
 EARLIEST_WRITEABLE_TIME = datetime(2024, 4, 16, 0, 0, 0, 000, tzinfo=timezone.utc)
-MOCKED_LAST_UPDATE_TIME = datetime(2024, 4, 16, 15, 5, 11, 200, tzinfo=timezone.utc)
-#########################################################################################
-
-
-@pytest.fixture(scope="module", autouse=True)
-def os_vars_init(aws_props_init):
-    # Sets up necessary lambda OS vars
-    (account_id, region) = aws_props_init
-    stage_name = "teststage"
-    os.environ[
-        "IAMROLE_MONITORED_ACC_EXTRACT_METRICS"
-    ] = f"role-salmon-monitored-acc-extract-metrics-{stage_name}"
-    os.environ[
-        "TIMESTREAM_METRICS_DB_NAME"
-    ] = f"timestream-salmon-metrics-events-storage-{stage_name}"
-    os.environ["SETTINGS_S3_PATH"] = f"s3://s3-salmon-settings-{stage_name}/settings/"
-    os.environ["ALERTS_EVENT_BUS_NAME"] = f"eventbus-salmon-alerting-{stage_name}"
-
-
-@pytest.fixture(scope="module")
-def os_vars_values(os_vars_init):
-    settings_s3_path = os.environ["SETTINGS_S3_PATH"]
-    iam_role_name = os.environ["IAMROLE_MONITORED_ACC_EXTRACT_METRICS"]
-    timestream_metrics_db_name = os.environ["TIMESTREAM_METRICS_DB_NAME"]
-    alerts_event_bus_name = os.environ["ALERTS_EVENT_BUS_NAME"]
-
-    return (
-        settings_s3_path,
-        iam_role_name,
-        timestream_metrics_db_name,
-        alerts_event_bus_name,
-    )
-
-
-#########################################################################################
-class MockedSettings:
-    @classmethod
-    def from_s3_path(cls, base_path: str, iam_role_list_monitored_res: str = None):
-        return MockedSettings()
-
-    def get_monitoring_group_content(self, group_name: str) -> dict:
-        return {"content": "define in a specific test"}
-
-    def get_monitored_environment_props(self):
-        return "1234567890", "us-east-1"
-
-
-@pytest.fixture(scope="module")
-def mock_settings():
-    with patch("lambda_extract_metrics.Settings", MockedSettings) as mocked_setting:
-        yield MockedSettings
-
-
-@pytest.fixture(scope="function")
-def mock_process_all_resources_by_env_and_type():
-    with patch(
-        "lambda_extract_metrics.process_all_resources_by_env_and_type"
-    ) as mocked_function:
-        yield mocked_function
-
-
-@pytest.fixture(scope="function")
-def mock_process_individual_resource():
-    with patch("lambda_extract_metrics.process_individual_resource") as mocked_function:
-        yield mocked_function
-
-
-@pytest.fixture(scope="function")
-def mock_metrics_extractor_from_provider(request):
-    include_set_result_ids = (
-        request.param.get("include_set_result_ids", False)
-        if hasattr(request, "param")
-        else False
-    )
-
-    with patch(
-        "lambda_extract_metrics.MetricsExtractorProvider.get_metrics_extractor"
-    ) as mock_get_extractor:
-        methods = ["get_last_update_time", "prepare_metrics_data", "write_metrics"]
-        if include_set_result_ids:
-            methods.append("set_result_ids")
-
-        mocked_extractor = MagicMock(spec=methods)
-
-        mocked_extractor.get_last_update_time.return_value = MOCKED_LAST_UPDATE_TIME
-        mocked_extractor.prepare_metrics_data.return_value = (
-            ["record1", "record2"],
-            ["common_attr1", "common_attr2"],
-        )
-
-        if "set_result_ids" in methods:
-            mocked_extractor.set_result_ids.return_value = TEST_DQ_RESULT_IDS
-
-        mock_get_extractor.return_value = mocked_extractor
-
-        yield mocked_extractor
-
-
-# @pytest.fixture(scope="function")
-# def mock_timestream_writer():
-#     mocked_timestream_writer = MagicMock()
-#     mocked_timestream_writer.get_earliest_writeable_time_for_table.return_value = (
-#         EARLIEST_WRITEABLE_TIME
-#     )
-
-#     with patch(
-#         "lambda_extract_metrics.TimestreamTableWriter", mocked_timestream_writer
-#     ) as mock_get_extractor:
-#         yield mocked_timestream_writer
-
-
-@pytest.fixture(scope="function")
-def mock_metrics_storage_earliest_writeable_time():
-    mocked_metrics_storage = MagicMock()
-    mocked_metrics_storage.get_earliest_writeable_time_for_table.return_value = (
-        EARLIEST_WRITEABLE_TIME
-    )
-
-    with patch(
-        "lambda_extract_metrics.TimestreamMetricsStorage", mocked_metrics_storage
-    ) as mock_get_extractor:
-        yield mocked_metrics_storage
-
-
-@pytest.fixture(scope="function")
-def mock_boto3_client_creator():
-    mocked_boto3_client_creator = MagicMock()
-    mocked_boto3_client_creator.account_id.return_value = "1234567890"
-    mocked_boto3_client_creator.region_id.return_value = "us-east-1"
-
-    with patch(
-        "lambda_extract_metrics.Boto3ClientCreator", mocked_boto3_client_creator
-    ) as mock_get_extractor:
-        yield mocked_boto3_client_creator
 
 
 #########################################################################################
